aac8901a34e01831e659861e995b32a90aa5c1e9
walid younis <walid.younis.90@gmail.com>
2020-08-17 22:26:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
 
-  array = [1, 2, 3]
-   new_numbers = []
-   array.length.times do |index|
+array = [1, 2, 3]
+new_numbers = []
+array.length.times do |index|
      new_numbers.push(array[index] * array[index])
    end
    puts new_numbers
